c1f018eda94a0eaa5130988c36438a4514b45fd2
roman.koval <roman.koval@powercode.us>
2020-11-30 22:29:06 +0200

    add delete curse secure

--- routes/courses.js
+++ routes/courses.js
@@ -5,8 +5,18 @@ const auth = require('../middleware/auth')
 const router = Router()
 
 router.get('/', async (req, res) => {
-    const courses = await Course.find()
-    res.render('courses', { courses })
+
+    try {
+        const courses = await Course.find().populate("userId", "email name")
+        res.render('courses', { 
+            courses,
+            userId: req?.user._id.toString() || null
+        
+        })
+        
+    } catch (error) {
+        console.log(error)
+    }
 })
 
 
@@ -16,24 +26,49 @@ router.get('/:id', async (req, res) => {
 })
 
 router.get('/:id/edit', auth, async (req, res) => {
-    if (!req.query.allow) {
-        return res.redirect('/')
+    try {
+        if (!req.query.allow) {
+            return res.redirect('/')
+        }
+
+        
+        const course = await Course.findById(req.params.id)
+        if(course.userId.toString() !== req.user._id.toString()){
+           return res.redirect('/courses')
+
+        }
+        res.render('course-edit', { course })
+        
+    } catch (error) {
+        console.log(error)
     }
-    const course = await Course.findById(req.params.id)
-    res.render('course-edit', { course })
+  
 })
 
 
 router.post('/edit', auth, async (req, res) => {
-    const { title, price, img, id } = req.body
 
-    await Course.findByIdAndUpdate(id, { title, price, img })
-    res.redirect('/courses')
+    try {
+        const { title, price, img, id } = req.body
+    
+        if(id.toString() === req.user._id.toString()){
+            return res.redirect('/courses')
+         }
+
+        await Course.findByIdAndUpdate(id, { title, price, img })
+        res.redirect('/courses')
+        
+    } catch (error) {
+        console.log(error)
+    }   
 })
 
 router.post('/remove', auth, async (req, res) => {
     try {
-        await Course.deleteOne({ _id: req.body.id })
+        await Course.deleteOne({ 
+            _id: req.body.id,
+            userId: req.user._id
+        })
         res.redirect('/courses')
     } catch (error) {
         console.log(error)
