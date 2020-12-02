a4edbbce2f935df78ecb7fa91dbeffc3cbf7d81a
roman.koval <romariooo27@gmail.com>
2020-12-02 22:23:39 +0200

    move smtp to keys

--- utils/nodeMailer.js
+++ utils/nodeMailer.js
@@ -1,25 +1,22 @@
-const nodemailer = require('nodemailer');
-const keys = require('../keys')
-
+const nodemailer = require("nodemailer");
+const keys = require("../keys");
 
 module.exports = async function (mailOptions) {
+  const transporter = nodemailer.createTransport({
+    host: keys.SMTP_HOST,
+    port: keys.SMTP_PORT,
+    secure: true,
+    auth: {
+      user: keys.EMAIL_FROM,
+      pass: keys.EMAIL_PASS,
+    },
+  });
 
-    const transporter = nodemailer.createTransport({
-        host: 'smtp.ukr.net',
-        port: 2525,
-        secure: true,
-        auth: {
-            user: keys.EMAIL_FROM,
-            pass: keys.EMAIL_PASS
-        }
-    });
-
-    return transporter.sendMail(mailOptions, function (error, info) {
-        if (error) {
-            console.log(error);
-        } else {
-            console.log('Email sent: ' + info.response);
-        }
-    });
-}
-
+  return transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Email sent: " + info.response);
+    }
+  });
+};
